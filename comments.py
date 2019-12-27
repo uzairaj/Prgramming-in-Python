4a5e1bf9864e3cb1698f7b1eb0b568745477ee11
uzairaj <39025103+uzairaj@users.noreply.github.com>
2019-12-27 05:06:12 +0500

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,9 @@
+#print('Hello World')
+
+print("It's a small world out there")
+
+'''
+We just learn how to write comments in python
+we can use single and double quotes when priniting anything
+
+'''
