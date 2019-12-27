003f5f250b5008bc60b7a5aa1cac99a477bfbd2f
uzairaj <39025103+uzairaj@users.noreply.github.com>
2019-12-27 05:20:36 +0500

    Delete comments.py

--- comments.py
+++
@@ -1,9 +0,0 @@
-#print('Hello World')
-
-print("It's a small world out there")
-
-'''
-We just learn how to write comments in python
-we can use single and double quotes when priniting anything
-
-'''
