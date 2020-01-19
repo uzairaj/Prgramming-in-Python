d8f77b0c0de45424882444665390d96cb945cfa7
uzairaj <39025103+uzairaj@users.noreply.github.com>
2020-01-19 21:07:21 +0500

    Add files via upload

---
+++ Conditional Logic/conditional_logic.py
@@ -0,0 +1,63 @@
+# Example 1
+
+
+first_number = input('Enter first number: ')
+second_number = input('Enter second number: ')
+
+if first_number > second_number:
+    print('Number 1 is greater')
+
+
+# Example 2
+
+first_number = input('Enter first number: ')
+second_number = input('Enter second number: ')
+
+if first_number > second_number:
+    print('Number 1 is greater')
+else:
+    print('Number 2 is greater')
+
+    
+# Example 3
+
+a = 100
+b= 100
+
+if a>b:
+    print('a is greater')
+elif a==b:
+    print('both are equal')
+else:
+    print('b is greater')
+
+
+# Example 4
+
+num = input("Enter a number: ") # input
+num = float(num) # typecasts string value into float
+
+if num > 0:
+    print("Positive number")
+
+elif num == 0:
+    print("Zero")
+       
+else:
+    print("Negative number")
+
+
+# Example 5
+num1 = 20
+num2 = 33
+num3 = 40
+if a > b and c > a:
+  print("Both conditions are True")
+
+
+# Example 6
+num1 = 20
+num2 = 33
+num3 = 40
+if a > b or c > a:
+  print("One condition is true")
